6fe623b2e23942c8cced07768b94d7f3bdccb252
Matheus Guedes de Andrade <matheusgandrade@poli.ufrj.br>
2017-12-08 16:11:35 -0200

    kmeans first version

---
+++ clustering/__init__.py
@@ -0,0 +1,82 @@
+import numpy as np
+
+__all__ = ["IterativeClustering"]
+
+
+# this is to be used as a base class
+class IterativeClustering:
+
+    def __init__(self, error):
+        self.accepted_training_error = error
+
+
+    def compute_clusters(self, clusters, data, init=None):
+        self.clusters = clusters
+        if init is None:
+            self.random_init(data, clusters)
+        else:
+            self.centers = init
+
+        error = np.inf
+        proceed = True
+        assignments = np.array([])
+        while proceed:
+            e2, assignments = self.expect(data)
+            self.optimize(data, assignments)
+            proceed = self.proceed(error, e2)
+            error = e2
+        return assignments
+
+
+    # phase: Expectation
+    def expect(self, data):
+        return None
+
+
+    # phase Maximization
+    def optimize(self, data):
+        return None
+
+
+    # to make stop criteria parametrizable
+    def proceed(self, e1, e2):
+        # return e1 > self.accepted_training_error and e1 != e2
+        return e1 != e2
+
+
+    # initialization with 
+    def random_init(self, data, clusters):
+        self.centers = data[np.random.random_integers(0, len(data), clusters)]
+
+
+
+class KMeans(IterativeClustering):
+
+    def __init__(self):
+        super(KMeans, self).__init__(0.0001)
+
+    def expect(self, data):
+        assignments = np.zeros(len(data), dtype=np.uint32)
+        acceptance = 0
+        for x in range(len(data)):
+            dist = np.inf
+            for k in range(self.clusters):
+                dist_vec = data[x] - self.centers[k]
+                dist_to_c = np.dot(dist_vec, dist_vec) ** 0.5
+                if dist_to_c ** 0.5 < dist: # update the best assignment
+                    dist = dist_to_c
+                    assignments[x] = k
+            acceptance += dist
+        return (acceptance, assignments)
+
+
+    # update cluster centers using assigned sample average
+    def optimize(self, data, assignments): 
+        centers = np.zeros((len(self.centers), len(data[0])))
+        counters = np.zeros(len(self.centers))
+        for i in range(len(data)):
+            centers[assignments[i]] += data[i]
+            counters[assignments[i]] += 1
+        for i in range(len(centers)):
+            centers[i] /= counters[i]
+        self.centers = centers
---
+++ k_means.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+from cross_validation import *
+from plot import *
+from error_functions import MSE
+import sys
+import pickle
+import clustering
+
+cast = lambda x: [int(x[0]), float(x[1]), float(x[2]), float(x[3])]
+raw_data = read_data("datasets/vo2-dataset.txt", cast)
+
+kmeans = clustering.KMeans()
+classified_data = kmeans.compute_clusters(3, raw_data[1])
+print(kmeans.centers)
+
+classified_data = kmeans.compute_clusters(4, raw_data[1])
+print(kmeans.centers)
